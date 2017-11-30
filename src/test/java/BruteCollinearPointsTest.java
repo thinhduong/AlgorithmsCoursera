792b4816f2017bf4a55b6aeb20ef10dad3adadef
thinhduong <thinhj_007@yahoo.com>
2017-11-30 17:47:39 +0700

    Week 3

---
+++ src/test/java/BruteCollinearPointsTest.java
@@ -0,0 +1,49 @@
+import org.junit.Test;
+
+
+import static org.junit.Assert.assertEquals;
+
+public class BruteCollinearPointsTest {
+    @Test
+    public void test1() {
+        int n = 8;
+        Point[] points = new Point[n];
+        points[0] = new Point(10000, 0);
+        points[1] = new Point(0, 10000);
+        points[2] = new Point(3000, 7000);
+        points[3] = new Point(7000   , 3000);
+        points[4] = new Point(20000  , 21000);
+        points[5] = new Point(3000   , 4000);
+        points[6] = new Point(14000     , 15000);
+        points[7] = new Point(6000      , 7000);
+
+        BruteCollinearPoints bruteCollinearPoints = new BruteCollinearPoints(points);
+
+        LineSegment[] ret = bruteCollinearPoints.segments();
+
+        assertEquals(ret.length, 2);
+        assertEquals(ret[0].toString(), "(10000, 0) -> (0, 10000)");
+        assertEquals(ret[1].toString(), "(3000, 4000) -> (20000, 21000)");
+    }
+
+    @Test
+    public void test2() {
+        int n = 8;
+        Point[] points = new Point[n];
+        points[0] = new Point(10000, 0);
+        points[1] = new Point(0, 10000);
+        points[2] = new Point(3000, 7000);
+        points[3] = new Point(7000   , 3000);
+        points[4] = new Point(20000  , 21000);
+        points[5] = new Point(3000   , 4000);
+        points[6] = new Point(14000     , 15000);
+        points[7] = new Point(6000      , 7000);
+
+        FastCollinearPoints fastCollinearPoints = new FastCollinearPoints(points);
+        LineSegment[] ret = fastCollinearPoints.segments();
+
+        assertEquals(ret.length, 2);
+        assertEquals(ret[0].toString(), "(3000, 4000) -> (20000, 21000)");
+        assertEquals(ret[1].toString(), "(0, 10000) -> (10000, 0)");
+    }
+}
